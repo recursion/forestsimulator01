18da152ce07aafd29da7188ff7d782c4fdc0b988
Michael Symmes <msymmes@gmail.com>
2015-09-25 18:34:07 -0400

    Added code to disable scrolling.

--- src/modules/game.js
+++ src/modules/game.js
@@ -5,7 +5,6 @@ import {Mob} from './mob.js';
 import {Map} from './map.js';
 
 const TILESIZE = 16;
-
 // create a game module for handling boilerplate
 // game stuff (init, load assets, switch levels?
 export class Game{
@@ -29,6 +28,7 @@ export class Game{
     window.addEventListener("keyup", this.keyboardEventsHandler.keysReleased.bind(this.keyboardEventsHandler), false);
     window.addEventListener("resize", this.setCanvasSize, false);
 
+    disableScroll();
 
     // load assets
     this.player = new Mob(this.map, 8, 8, "white");
@@ -117,3 +117,41 @@ export class Game{
     }, 1000 / 60);
   }
 }
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+/*
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+*/
